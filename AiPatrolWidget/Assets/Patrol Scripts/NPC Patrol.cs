042268e031416933d0dbb55631aa516e2fb0096c
Dead245 <116427900+Dead245@users.noreply.github.com>
2024-09-03 22:32:46 -0500

    Created simple NPC

--- AiPatrolWidget/Assets/Patrol Scripts/NPC Patrol.cs
+++ AiPatrolWidget/Assets/Patrol Scripts/NPC Patrol.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Splines;
 
 public class NPCPatrol : MonoBehaviour
 {
     [SerializeField]
     GameObject npc;
+
+    [SerializeField]
+    SplineContainer splinePath;
+
 
     // Start is called before the first frame update
     void Start()
