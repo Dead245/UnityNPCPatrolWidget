768d64dcb52ba724fbb9918edbcb4948bb02d173
Dead245 <116427900+Dead245@users.noreply.github.com>
2024-09-03 23:31:24 -0500

    Got 1 Npc to follow path
    Rotates itself on its' y axis as it follows the spline.

--- AiPatrolWidget/Assets/Patrol Scripts/NPC Patrol.cs
+++ AiPatrolWidget/Assets/Patrol Scripts/NPC Patrol.cs
@@ -1,26 +1,65 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Splines;
 
 public class NPCPatrol : MonoBehaviour
 {
     [SerializeField]
     GameObject npc;
+    float instantiationYOffset = 1f;
+    GameObject spawnedNPC;
 
     [SerializeField]
     SplineContainer splinePath;
 
+    float walkSpeed = 5f;
+    float stepLength = 0.05f;
+    float splineLength;
 
-    // Start is called before the first frame update
+    float distancePercent = 0;
+    Vector3 currentPos, direction;
+    Vector3 firstPos, firstDir;
+
     void Start()
     {
-        
+        firstPos = splinePath.EvaluatePosition(0);
+        firstPos.y += instantiationYOffset;
+        firstDir = getDirectionVector(0f, 0.01f);
+        spawnedNPC = Instantiate(npc, firstPos, Quaternion.LookRotation(firstDir));
+        splineLength = splinePath.CalculateLength();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        distancePercent += walkSpeed * Time.deltaTime / splineLength;
+
+        if (distancePercent > 1.0f) { return; }
+
+        Vector3 newPos = splinePath.EvaluatePosition(distancePercent);
+        newPos.y += instantiationYOffset;
+        spawnedNPC.transform.position = newPos;
+
+        direction = getDirectionVector(distancePercent, distancePercent + stepLength);
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        rotation.x = spawnedNPC.transform.rotation.x;
+        rotation.z = spawnedNPC.transform.rotation.z;
+        spawnedNPC.transform.rotation = rotation;
+
+    }
+    Vector3 getDirectionVector(float firstPercent, float secondPercent)
+    {
+        Vector3 direction;
+        Vector3 _firstPos, _secondPos;
+
+        _firstPos = splinePath.EvaluatePosition(firstPercent);
+        _secondPos = splinePath.EvaluatePosition(secondPercent);
+
+        direction = _secondPos - _firstPos;
+
+        return direction;
     }
 }
